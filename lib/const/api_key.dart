e279ca534467d03ec6fcccb14ccbfb00f5caf766
sssulaimon <ssalaudeen10@gmail.com>
2023-06-27 18:34:49 +0100

    removed my api key

--- lib/const/api_key.dart
+++
@@ -1,5 +0,0 @@
-class ApiKey {
-  static String apiKey = 'a230630bbc58e1a0e2da2e984b3da0e5';
-  static String token =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjMwNjMwYmJjNThlMWEwZTJkYTJlOTg0YjNkYTBlNSIsInN1YiI6IjYwNTMyNTEwZmE3OGNkMDBhMzFkNDg4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L1ShUP-azQo93n03699jSQ5JKFLqEXSa0k4cOfOhFDQ';
-}
